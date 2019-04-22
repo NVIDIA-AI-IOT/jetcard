3511d882ad72095995e4b174eb81169fdb44d500
John Welsh <jwelsh@nvidia.com>
2019-04-22 13:41:54 -0700

    script

---
+++ jetkit/create_stats_service.py
@@ -0,0 +1,33 @@
+import argparse
+import getpass
+import os
+
+STATS_SERVICE_TEMPLATE = """
+[Unit]
+Description=JetKit stats display service
+
+[Service]
+Type=simple
+User=%s
+ExecStart=/bin/sh -c "python3 -m jetkit.display_stats"
+WorkingDirectory=%s
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+"""
+
+STATS_SERVICE_NAME = 'jetkit_display_stats'
+
+
+def get_stats_service():
+    return STATS_SERVICE_TEMPLATE % (getpass.getuser(), os.environ['HOME'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', default='jetkit_display_stats.service')
+    args = parser.parse_args()
+
+    with open(args.output, 'w') as f:
+        f.write(get_stats_service())
