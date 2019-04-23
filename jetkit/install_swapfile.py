6921f8308a6c62af2998c614e87b44c4f1c87cb6
John Welsh <jwelsh@nvidia.com>
2019-04-22 18:55:13 -0700

    rem inst

--- jetkit/install_swapfile.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-if __name__ == '__main__':
-    subprocess.call(['sudo fallocate -l 4G /var/swapfile'])
-    subprocess.call(['sudo chmod 600 /var/swapfile'])
-    subprocess.call(['sudo mkswap /var/swapfile'])
-    subprocess.call(['sudo swapon /var/swapfile'])
-    subprocess.call(['sudo bash -c echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab'])
--- jetkit/install_tensorflow.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-if __name__ == '__main__':
-    subprocess.call(['sudo apt-get install libhdf5-serial-dev hdf5-tools'])
-    subprocess.call(['sudo apt-get install python3-pip'])
-    subprocess.call(['sudo apt-get install zlib1g-dev zip libjpeg8-dev libhdf5-dev'])
-    subprocess.call(['sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker grpcio six mock requests gast h5py astor termcolor'])
-    subprocess.call(['sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu'])
