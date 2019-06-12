385ab48d485bbfb02a53bacf6fd8bc908b4d6211
Chitoku YATO <cyato@nvidia.com>
2019-06-12 00:01:04 -0700

    Change sudo password handling

--- install.sh
+++ install.sh
@@ -2,67 +2,75 @@
 
 set -e
 
-password=$1
+echo "### Keep sudo session alive..."
+sudo -v
+while true; do
+	# Update timestamp
+	sudo -nv; sleep 120
+	# Exit when the parent process is not running any more.
+	kill -0 $$ 2>/dev/null || exit
+done &
 
-# enable i2c permissions
-echo $password | sudo -S usermod -aG i2c $USER
 
-# install pip and some apt dependencies
-echo $password | sudo -S apt-get update
-echo $password | sudo -S apt install -y python3-pip python3-pil python3-smbus python3-matplotlib cmake
-echo $password | sudo -S pip3 install -U --upgrade numpy
+echo "### enable i2c permissions"
+sudo usermod -aG i2c $USER
 
-# install tensorflow
-echo $password | sudo -S apt-get install -y libhdf5-serial-dev hdf5-tools
-echo $password | sudo -S apt-get install -y zlib1g-dev zip libjpeg8-dev libhdf5-dev
-echo $password | sudo -S pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker grpcio six mock requests gast h5py astor termcolor
-echo $password | sudo -S pip3 install -U --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu
+echo "### install pip and some apt dependencies"
+sudo apt-get update
+sudo apt install -y python3-pip python3-pil python3-smbus python3-matplotlib cmake
+sudo pip3 install -U --upgrade numpy
 
-# install pytorch
+echo "### install tensorflow"
+sudo apt-get install -y libhdf5-serial-dev hdf5-tools
+sudo apt-get install -y zlib1g-dev zip libjpeg8-dev libhdf5-dev
+sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker grpcio six mock requests gast h5py astor termcolor
+sudo pip3 install -U --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu
+
+echo "### install pytorch"
 wget https://nvidia.box.com/shared/static/veo87trfaawj5pfwuqvhl6mzc5b55fbj.whl -O torch-1.1.0a0+b457266-cp36-cp36m-linux_aarch64.whl
-echo $password | sudo -S pip3 install -U numpy torch-1.1.0a0+b457266-cp36-cp36m-linux_aarch64.whl
-echo $password | sudo -S pip3 install -U torchvision
+sudo pip3 install -U numpy torch-1.1.0a0+b457266-cp36-cp36m-linux_aarch64.whl
+sudo pip3 install -U torchvision
 
-# setup Jetson.GPIO
-echo $password | sudo -S groupadd -f -r gpio
-echo $password | sudo -S usermod -a -G gpio $USER
-echo $password | sudo -S cp /opt/nvidia/jetson-gpio/etc/99-gpio.rules /etc/udev/rules.d/
-echo $password | sudo -S udevadm control --reload-rules && sudo udevadm trigger
+echo "### setup Jetson.GPIO"
+sudo groupadd -f -r gpio
+sudo usermod -a -G gpio $USER
+sudo cp /opt/nvidia/jetson-gpio/etc/99-gpio.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && sudo udevadm trigger
 
-# install traitlets (master)
-echo $password | sudo -S python3 -m pip install git+https://github.com/ipython/traitlets@master
+echo "### install traitlets (master)"
+sudo python3 -m pip install git+https://github.com/ipython/traitlets@master
 
-# install jupyter lab
-echo $password | sudo -S apt install -y nodejs npm
-echo $password | sudo -S pip3 install -U jupyter jupyterlab
-echo $password | sudo -S jupyter labextension install @jupyter-widgets/jupyterlab-manager
-echo $password | sudo -S jupyter labextension install @jupyterlab/statusbar
+echo "### install jupyter lab"
+sudo apt install -y nodejs npm
+sudo pip3 install -U jupyter jupyterlab
+sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager
+sudo jupyter labextension install @jupyterlab/statusbar
 jupyter lab --generate-config
 jupyter notebook password
 
-# install jetcard
-echo $password | sudo -S python3 setup.py install
+echo "### install jetcard"
+sudo python3 setup.py install
 
-# install jetcard stats service
+echo "### install jetcard stats service"
 python3 -m jetcard.create_stats_service
-echo $password | sudo -S mv jetcard_stats.service /etc/systemd/system/jetcard_stats.service
-echo $password | sudo -S systemctl enable jetcard_stats
-echo $password | sudo -S systemctl start jetcard_stats
+sudo mv jetcard_stats.service /etc/systemd/system/jetcard_stats.service
+sudo systemctl enable jetcard_stats
+sudo systemctl start jetcard_stats
 
-# install jetcard jupyter service
+echo "### install jetcard jupyter service"
 python3 -m jetcard.create_jupyter_service
-echo $password | sudo -S mv jetcard_jupyter.service /etc/systemd/system/jetcard_jupyter.service
-echo $password | sudo -S systemctl enable jetcard_jupyter
-echo $password | sudo -S systemctl start jetcard_jupyter
+sudo mv jetcard_jupyter.service /etc/systemd/system/jetcard_jupyter.service
+sudo systemctl enable jetcard_jupyter
+sudo systemctl start jetcard_jupyter
 
-# make swapfile
-echo $password | sudo -S fallocate -l 4G /var/swapfile
-echo $password | sudo -S chmod 600 /var/swapfile
-echo $password | sudo -S mkswap /var/swapfile
-echo $password | sudo -S swapon /var/swapfile
-echo $password | sudo -S bash -c 'echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab'
+echo "### make swapfile"
+sudo fallocate -l 4G /var/swapfile
+sudo chmod 600 /var/swapfile
+sudo mkswap /var/swapfile
+sudo swapon /var/swapfile
+sudo bash -c 'echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab'
 
-# install TensorFlow models repository
+echo "### install TensorFlow models repository"
 git clone https://github.com/tensorflow/models
 cd models/research
 git checkout 5f4d34fc
@@ -70,6 +78,6 @@ wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/downlo
 # wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
 unzip protobuf.zip
 ./bin/protoc object_detection/protos/*.proto --python_out=.
-echo $password | sudo -S python3 setup.py install
+sudo python3 setup.py install
 cd slim
-echo $password | sudo -S python3 setup.py install
+sudo python3 setup.py install
