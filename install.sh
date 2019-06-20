4f911217306e93d64293fc6b75892532288edf5e
John Welsh <jwelsh@nvidia.com>
2019-06-20 12:52:49 -0700

    sudo hotfix + fix scripts to install

--- install.sh
+++ install.sh
@@ -4,66 +4,81 @@ set -e
 
 password=$1
 
+# keep sudo alive
+echo $password | sudo -v -S
+while true; do
+	# Update timestamp
+	sudo -nv; sleep 120
+	# Exit when the parent process is not running any more.
+	kill -0 $$ 2>/dev/null || exit
+done &
+
+# fix NTP server
+./scripts/jetson_add_ntp_server.sh
+
+# fix USB device mode
+./scripts/jetson_fix_usb_device_mode.sh
+
 # enable i2c permissions
-echo $password | sudo -S usermod -aG i2c $USER
+sudo usermod -aG i2c $USER
 
 # install pip and some apt dependencies
-echo $password | sudo -S apt-get update
-echo $password | sudo -S apt install -y python3-pip python3-pil python3-smbus python3-matplotlib cmake
-echo $password | sudo -S pip3 install flask
-echo $password | sudo -S pip3 install -U --upgrade numpy
+sudo apt-get update
+sudo apt install -y python3-pip python3-pil python3-smbus python3-matplotlib cmake
+sudo pip3 install flask
+sudo pip3 install -U --upgrade numpy
 
 # install tensorflow
-echo $password | sudo -S apt-get install -y libhdf5-serial-dev hdf5-tools
-echo $password | sudo -S apt-get install -y zlib1g-dev zip libjpeg8-dev libhdf5-dev
-echo $password | sudo -S pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker grpcio six mock requests gast h5py astor termcolor
-echo $password | sudo -S pip3 install -U --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu
+sudo apt-get install -y libhdf5-serial-dev hdf5-tools
+sudo apt-get install -y zlib1g-dev zip libjpeg8-dev libhdf5-dev
+sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker grpcio six mock requests gast h5py astor termcolor
+sudo pip3 install -U --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu
 
 # install pytorch
 wget https://nvidia.box.com/shared/static/veo87trfaawj5pfwuqvhl6mzc5b55fbj.whl -O torch-1.1.0a0+b457266-cp36-cp36m-linux_aarch64.whl
-echo $password | sudo -S pip3 install -U numpy torch-1.1.0a0+b457266-cp36-cp36m-linux_aarch64.whl
-echo $password | sudo -S pip3 install -U torchvision
+sudo pip3 install -U numpy torch-1.1.0a0+b457266-cp36-cp36m-linux_aarch64.whl
+sudo pip3 install -U torchvision
 
 # setup Jetson.GPIO
-echo $password | sudo -S groupadd -f -r gpio
-echo $password | sudo -S usermod -a -G gpio $USER
-echo $password | sudo -S cp /opt/nvidia/jetson-gpio/etc/99-gpio.rules /etc/udev/rules.d/
-echo $password | sudo -S udevadm control --reload-rules && sudo udevadm trigger
+sudo groupadd -f -r gpio
+sudo usermod -a -G gpio $USER
+sudo cp /opt/nvidia/jetson-gpio/etc/99-gpio.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && sudo udevadm trigger
 
 # install traitlets (master)
-echo $password | sudo -S python3 -m pip install git+https://github.com/ipython/traitlets@master
+sudo python3 -m pip install git+https://github.com/ipython/traitlets@master
 
 # install jupyter lab
-echo $password | sudo -S apt install -y nodejs npm
-echo $password | sudo -S pip3 install -U jupyter jupyterlab
-echo $password | sudo -S jupyter labextension install @jupyter-widgets/jupyterlab-manager
-echo $password | sudo -S jupyter labextension install @jupyterlab/statusbar
+sudo apt install -y nodejs npm
+sudo pip3 install -U jupyter jupyterlab
+sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager
+sudo jupyter labextension install @jupyterlab/statusbar
 jupyter lab --generate-config
 
 # set jupyter password
 python3 -c "from notebook.auth.security import set_password; set_password('$password', '$HOME/.jupyter/jupyter_notebook_config.json')"
 
 # install jetcard
-echo $password | sudo -S python3 setup.py install
+sudo python3 setup.py install
 
 # install jetcard display service
 python3 -m jetcard.create_display_service
-echo $password | sudo -S mv jetcard_display.service /etc/systemd/system/jetcard_display.service
-echo $password | sudo -S systemctl enable jetcard_display
-echo $password | sudo -S systemctl start jetcard_display
+sudo mv jetcard_display.service /etc/systemd/system/jetcard_display.service
+sudo systemctl enable jetcard_display
+sudo systemctl start jetcard_display
 
 # install jetcard jupyter service
 python3 -m jetcard.create_jupyter_service
-echo $password | sudo -S mv jetcard_jupyter.service /etc/systemd/system/jetcard_jupyter.service
-echo $password | sudo -S systemctl enable jetcard_jupyter
-echo $password | sudo -S systemctl start jetcard_jupyter
+sudo mv jetcard_jupyter.service /etc/systemd/system/jetcard_jupyter.service
+sudo systemctl enable jetcard_jupyter
+sudo systemctl start jetcard_jupyter
 
 # make swapfile
-echo $password | sudo -S fallocate -l 4G /var/swapfile
-echo $password | sudo -S chmod 600 /var/swapfile
-echo $password | sudo -S mkswap /var/swapfile
-echo $password | sudo -S swapon /var/swapfile
-echo $password | sudo -S bash -c 'echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab'
+sudo fallocate -l 4G /var/swapfile
+sudo chmod 600 /var/swapfile
+sudo mkswap /var/swapfile
+sudo swapon /var/swapfile
+sudo bash -c 'echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab'
 
 # install TensorFlow models repository
 git clone https://github.com/tensorflow/models
@@ -73,25 +88,25 @@ wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/downlo
 # wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
 unzip protobuf.zip
 ./bin/protoc object_detection/protos/*.proto --python_out=.
-echo $password | sudo -S python3 setup.py install
+sudo python3 setup.py install
 cd slim
-echo $password | sudo -S python3 setup.py install
+sudo python3 setup.py install
 
 # disable syslog to prevent large log files from collecting
-echo $password | sudo -S service rsyslog stop
-echo $password | sudo -S systemctl disable rsyslog
+sudo service rsyslog stop
+sudo systemctl disable rsyslog
 
 # install jupyter_clickable_image_widget
-echo $password | sudo npm install -g typescript
+sudo npm install -g typescript
 git clone https://github.com/jaybdub/jupyter_clickable_image_widget
 cd jupyter_clickable_image_widget
 
 # allow next command to fail
 set +e
-echo $password | sudo python3 setup.py build
+sudo python3 setup.py build
 
 set -e
-echo $password | sudo npm run build
-echo $password | sudo pip3 install .
-echo $password | sudo jupyter labextension install .
-echo $password | sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager
+sudo npm run build
+sudo pip3 install .
+sudo jupyter labextension install .
+sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager
