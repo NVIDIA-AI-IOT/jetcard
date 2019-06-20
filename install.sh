80869e7ae423f2766e5c4f5c90e8d42108c0dbc4
John Welsh <jwelsh@nvidia.com>
2019-06-20 15:37:36 -0700

    tensorflow added

--- install.sh
+++ install.sh
@@ -25,14 +25,14 @@ sudo usermod -aG i2c $USER
 # install pip and some apt dependencies
 sudo apt-get update
 sudo apt install -y python3-pip python3-pil python3-smbus python3-matplotlib cmake
+sudo pip3 install -U pip
 sudo pip3 install flask
 sudo pip3 install -U --upgrade numpy
 
 # install tensorflow
-sudo apt-get install -y libhdf5-serial-dev hdf5-tools
-sudo apt-get install -y zlib1g-dev zip libjpeg8-dev libhdf5-dev
-sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker grpcio six mock requests gast h5py astor termcolor
-# sudo pip3 install -U --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu
+sudo apt-get install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev
+sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker six mock requests gast h5py astor termcolor protobuf keras-applications keras-preprocessing wrapt google-pasta
+sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu
 
 # install pytorch
 wget https://nvidia.box.com/shared/static/veo87trfaawj5pfwuqvhl6mzc5b55fbj.whl -O torch-1.1.0a0+b457266-cp36-cp36m-linux_aarch64.whl
@@ -81,16 +81,16 @@ sudo swapon /var/swapfile
 sudo bash -c 'echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab'
 
 # install TensorFlow models repository
-# git clone https://github.com/tensorflow/models
-# cd models/research
-# git checkout 5f4d34fc
-# wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-aarch_64.zip
-# # wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
-# unzip protobuf.zip
-# ./bin/protoc object_detection/protos/*.proto --python_out=.
-# sudo python3 setup.py install
-# cd slim
-# sudo python3 setup.py install
+git clone https://github.com/tensorflow/models
+cd models/research
+git checkout 5f4d34fc
+wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-aarch_64.zip
+# wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
+unzip protobuf.zip
+./bin/protoc object_detection/protos/*.proto --python_out=.
+sudo python3 setup.py install
+cd slim
+sudo python3 setup.py install
 
 # disable syslog to prevent large log files from collecting
 sudo service rsyslog stop
