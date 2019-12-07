aafa9ec183d946729f7b8998d336b64807308671
waveshare <36142105+waveshare@users.noreply.github.com>
2019-12-08 00:50:08 +0800

    Update stats.py

--- jetcard/stats.py
+++ jetcard/stats.py
@@ -27,10 +27,21 @@ from PIL import ImageDraw
 from PIL import ImageFont
 from .utils import ip_address
 from jetcard import ads1115
-
+from jetcard import ina219
+import os
 import subprocess
 
-ads = ads1115.ADS1115()
+adress = os.popen("i2cdetect -y -r 1 0x48 0x48 | egrep '48' | awk '{print $2}'").read()
+if(adress=='48\n'):
+    ads = ads1115.ADS1115()
+else:
+    ads = None
+
+adress = os.popen("i2cdetect -y -r 1 0x41 0x41 | egrep '41' | awk '{print $2}'").read()
+if(adress=='41\n'):
+    ina = ina219.INA219(addr=0x41)
+else:
+    ina = None
 
 # 128x32 display with hardware I2C:
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=None, i2c_bus=1, gpio=1) # setting gpio to 1 is hack to avoid platform detection
@@ -67,7 +78,6 @@ font = ImageFont.load_default()
 
 
 while True:
-    value=ads.readVoltage(4)/1000.0
     
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
@@ -81,11 +91,41 @@ while True:
     Disk = subprocess.check_output(cmd, shell = True )
 
     # Write two lines of text.
+    if(ina != None):
+        if ip_address('eth0') is not None:
+            draw.text((x, top), 'IP: ' + str(ip_address('eth0')), font=font, fill=255)
+        elif ip_address('wlan0') is not None:
+            draw.text((x, top), 'IP: ' + str(ip_address('wlan0')), font=font, fill=255)
+        else:
+            draw.text((x, top), 'IP: not available', font=font, fill=255)
 
-    draw.text((x, top),       "eth0:" + str(ip_address('eth0')),  font=font, fill=255)
-    draw.text((x, top+8),     "wlan0:" + str(ip_address('wlan0')), font=font, fill=255)
-    draw.text((x, top+16),    str(MemUsage.decode('utf-8')),  font=font, fill=255)
-    draw.text((x, top+25),    str(Disk.decode('utf-8')) + (" %.1f")%value,  font=font, fill=255)
+        bus_voltage = ina.getBusVoltage_V()        # voltage on V- (load side)
+        current = ina.getCurrent_mA()              # current in mA
+        p = bus_voltage/12.6*100
+        if(p > 100):p = 100
+        if(current > 30):
+            Charge = not Charge
+        else:
+            Charge = False
+
+        if(Charge == False):
+            draw.text((120, top), ' ', font=font, fill=255)
+        else:
+            draw.text((120, top), '*', font=font, fill=255)
+        draw.text((x, top+8), ("%.1fV")%bus_voltage + ("  %.2fA")%(current/1000) + ("  %2.0f%%")%p, font=font, fill=255)    
+        draw.text((x, top+16),    str(MemUsage.decode('utf-8')),  font=font, fill=255)
+        draw.text((x, top+25),    str(Disk.decode('utf-8')),  font=font, fill=255)
+    elif(ads != None):
+        value=ads.readVoltage(4)/1000.0
+        draw.text((x, top),       "eth0: " + str(get_ip_address('eth0')),  font=font, fill=255)
+        draw.text((x, top+8),     "wlan0: " + str(get_ip_address('wlan0')), font=font, fill=255)
+        draw.text((x, top+16),    str(MemUsage.decode('utf-8')),  font=font, fill=255)
+        draw.text((x, top+25),    str(Disk.decode('utf-8')) + (" %.1f")%value,  font=font, fill=255)
+    else:
+        draw.text((x, top),       "eth0: " + str(get_ip_address('eth0')),  font=font, fill=255)
+        draw.text((x, top+8),     "wlan0: " + str(get_ip_address('wlan0')), font=font, fill=255)
+        draw.text((x, top+16),    str(MemUsage.decode('utf-8')),  font=font, fill=255)
+        draw.text((x, top+25),    str(Disk.decode('utf-8')),  font=font, fill=255)
 
     # Display image.
     disp.image(image)
