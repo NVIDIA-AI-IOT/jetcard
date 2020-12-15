88cb874b2586006a84e818b85509423513421bab
John Welsh <jaybdub@users.noreply.github.com>
2020-12-15 11:46:19 -0800

    removed strict versions for tensorflow dependencies

--- install.sh
+++ install.sh
@@ -144,9 +144,8 @@ fi
 if [[ $install_tensorflow =~ ^[Yy]$ ]];
 then
 	apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
-	apt-get install python3-pip
-	pip3 install -U pip testresources setuptools==49.6.0
-	pip3 install -U numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+	pip3 install -U testresources setuptools
+	pip3 install -U future mock h5py keras_preprocessing keras_applications gast futures protobuf pybind11
 	if [[ $tensorflow_version == 2 ]];
 	then
 		pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow
